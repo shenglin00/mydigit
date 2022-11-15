9bdd09d8170f5fe87c23824d03f36fbd85844f9b
shenglin00 <71120049+shenglin00@users.noreply.github.com>
2022-11-15 18:45:12 +0800

    clean up to reduce code size

--- autosign.sh
+++ autosign.sh
@@ -16,78 +16,78 @@ ANSWER=""
 
 
 EXT_HEADER="Host: www.mydigit.cn"
-PORTAL="https://www.mydigit.cn/"
-SIGN_PAGE="https://www.mydigit.cn/k_misign-sign.html"
-LOGIN_PAGE="https://www.mydigit.cn/member.php?mod=logging&action=login&handlekey=login"
+BASE_URL="https://www.mydigit.cn"
+PORTAL="$BASE_URL/"
+SIGN_PAGE="$BASE_URL/k_misign-sign.html"
+LOGIN_PAGE="$BASE_URL/member.php?mod=logging&action=login&handlekey=login"
 CURL_COMMON='curl -A "$USER_AGENT" -H "$EXT_HEADER" -s -D -'
-CUT_COOKIE='(tolower($1)=="set-cookie:"){n=split($0,arr," *; *")
+
+# awk substr param a added length("\"")
+get_formhash() {
+  FORM_HASH=`grep formhash /tmp/autosign.$$ | grep -v jQuery | grep input \
+           | awk -F 'value=' '!(arr[$0]++){print substr($2,2,8)}'`
+}
+
+add_cookie() {
+  COOKIE=`awk '(tolower($1)=="set-cookie:"){n=split($0,arr," *; *")
               for(i=2;i<=n;i++){if(tolower(arr[i])=="max-age=0"){flag=1}}
               if(flag){split($2,arr,"=");print arr[1]"=,;"}
-              else{print $2};flag=false}'
-
-trim_cookie() {
+              else{print $2};flag=0}' << EOF
+$*
+EOF
+`" "$COOKIE
+  
+  # trim cookie
   COOKIE=`sed 's/ /\n/g' << EOF | awk -F '=' '!arr[$1]++' | grep -v '^[^=]*=,;$'
 $COOKIE
 EOF
 `
 }
 
-# awk substr param a added length("\"")
-get_formhash() {
-  FORM_HASH=`grep formhash /tmp/autosign.tmp | grep -v jQuery | grep input \
-           | awk -F 'value=' '!(arr[$0]++){print substr($2,2,8)}'`
-}
-
 # request homepage
-COOKIE=`$CURL_COMMON -o /dev/null $PORTAL | awk "$CUT_COOKIE"`
+add_cookie "`$CURL_COMMON -o /dev/null $PORTAL`"
 
 # get time from lastact
-LASTACT_TIME=`sed -n '/_lastact/{s/^[^=]*=//;s/%.*;$//p}' << EOF
+LASTACT_TIME=`sed -n '/_lastact/{s/^[^=]*=//;s/[^0-9].*;$//p}' << EOF
 $COOKIE
 EOF
 `
 
 # sendmail?
-COOKIE=`$CURL_COMMON -b "$(echo $COOKIE)" -e "$PORTAL" -o /dev/null \
-        https://www.mydigit.cn/home.php?mod=misc\&ac=sendmail\&rand=$LASTACT_TIME \
-      | awk "$CUT_COOKIE"`" "$COOKIE
-trim_cookie
+add_cookie "`$CURL_COMMON -b "$(echo $COOKIE)" -e "$PORTAL" -o /dev/null \
+           $BASE_URL/home.php?mod=misc\&ac=sendmail\&rand=$LASTACT_TIME`"
 
-rm -f /tmp/autosign.tmp
+rm -f /tmp/autosign.$$
 # request fwin_content_login
-COOKIE=`$CURL_COMMON -b "$(echo $COOKIE)" -e "$PORTAL" -o /tmp/autosign.tmp \
-        $LOGIN_PAGE\&infloat=yes\&inajax=1\&ajaxtarget=fwin_content_login \
-      | awk "$CUT_COOKIE"`" "$COOKIE
-trim_cookie
+add_cookie "`$CURL_COMMON -b "$(echo $COOKIE)" -e "$PORTAL" -o /tmp/autosign.$$ \
+           $LOGIN_PAGE\&infloat=yes\&inajax=1\&ajaxtarget=fwin_content_login`"
+
 # get loginhash and formhash
-LOGIN_HASH=`awk -F 'loginhash=' '$2{print substr($2,1,5)}' /tmp/autosign.tmp`
+LOGIN_HASH=`awk -F 'loginhash=' '$2{print substr($2,1,5)}' /tmp/autosign.$$`
 get_formhash
-rm -f /tmp/autosign.tmp
+rm -f /tmp/autosign.$$
 
 # login
-COOKIE=`$CURL_COMMON -b "$(echo $COOKIE)" -e "$PORTAL" -X POST -o /dev/null \
-        -d formhash=$FORM_HASH\&referer=$PORTAL\&username=$USER_NAME\&password=$PASSWORD\&questionid=$QUESTION_ID\&answer=$ANSWER \
-        $LOGIN_PAGE\&loginsubmit=yes\&loginhash=$LOGIN_HASH\&inajax=1 \
-      | awk "$CUT_COOKIE"`" "$COOKIE
-trim_cookie
+add_cookie "`$CURL_COMMON -b "$(echo $COOKIE)" -e "$PORTAL" -X POST -o /dev/null \
+           -d formhash=$FORM_HASH\&referer=$PORTAL\&username=$USER_NAME\&password=$PASSWORD\&questionid=$QUESTION_ID\&answer=$ANSWER \
+           $LOGIN_PAGE\&loginsubmit=yes\&loginhash=$LOGIN_HASH\&inajax=1`"
 
 sleep $DELAY_TIME
 # request sign page
-COOKIE=`$CURL_COMMON -b "$(echo $COOKIE)" -e "$PORTAL" -o /tmp/autosign.tmp $SIGN_PAGE \
-      | awk "$CUT_COOKIE"`" "$COOKIE
-trim_cookie
+add_cookie "`$CURL_COMMON -b "$(echo $COOKIE)" -e "$PORTAL" -o /tmp/autosign.$$ \
+           $SIGN_PAGE`"
 # get formhash
 get_formhash
-rm -f /tmp/autosign.tmp
+rm -f /tmp/autosign.$$
 
 # do sign
-COOKIE=`$CURL_COMMON -b "$(echo $COOKIE)" -e "$SIGN_PAGE" -o /dev/null \
-        https://www.mydigit.cn/plugin.php?id=k_misign:sign\&operation=qiandao\&formhash=$FORM_HASH\&format=empty \
-      | awk "$CUT_COOKIE"`" "$COOKIE
-trim_cookie
+add_cookie "`$CURL_COMMON -b "$(echo $COOKIE)" -e "$SIGN_PAGE" -o - \
+           $BASE_URL/plugin.php?id=k_misign:sign\&operation=qiandao\&formhash=$FORM_HASH\&format=empty
+           `"
+
 sleep $DELAY_TIME
 [ -z $SPACE_UID ] || 
   $CURL_COMMON -b "$(echo $COOKIE)" -e "$SIGN_PAGE" -o /dev/null \
-  https://www.mydigit.cn/home.php?mod=space\&uid=$SPACE_UID\&do=profile >/dev/null 2>&1
+  $BASE_URL/home.php?mod=space\&uid=$SPACE_UID\&do=profile >/dev/null 2>&1
 
 exit 0
